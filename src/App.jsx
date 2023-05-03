ba3c49468d0fea6faccbc670c3fec32961368bb1
Chris Vecchione <cwvecchione@gmail.com>
2023-05-02 20:53:09 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Table from './components/Table';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="departures">
+        <header>DEPARTURES</header>
+        <Table/>
+    </div>
+
+    );
+}
+
+export default App;
